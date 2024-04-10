aa48fff7ee396cc66635b5a07c72f71b78caf3a9
ahmed007dr <ahmed007eg@gmail.com>
2024-04-10 02:15:49 +0200

    create model for ML

---
+++ ML/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Iris(models.Model):
+    sepal_length = models.FloatField()
+    sepal_width = models.FloatField()
+    petal_length = models.FloatField()
+    petal_width = models.FloatField()
+
+    classifiction = models.CharField(max_length=20, blank=True, null=True)
+    def __str__(self):
+        return self.classifiction
