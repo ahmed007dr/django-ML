bc423002af6dadfa3e3d5601cb9a4ddb7563e02c
ahmed007dr <ahmed007eg@gmail.com>
2024-04-10 02:45:42 +0200

    a

--- ML/views.py
+++ ML/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from .models import Iris
 from .form import IrisForm
-# Create your views here.
 
 def predict(request):
 
     form = IrisForm()
+    
     return render(request,'predict.html',{'form':form})
